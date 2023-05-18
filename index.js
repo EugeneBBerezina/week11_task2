ef785e5fe026962978be0e3e5a2f77af29535cb1
EugeneBBerezina <hysteriajane@gmail.com>
2023-05-18 23:51:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let inputNumber = document.getElementById("number");
+let inputElem = document.getElementById("elem");
+let button = document.getElementById("result");
+button.addEventListener('click', function () {
+    inputElem.value = inputNumber.value ** 2
+});
+
+
+
+
+
+
+
+
